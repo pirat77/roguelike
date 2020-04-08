a34e5eab04bdf513b6d6da74911e97e8c2b5fbb3
pirat77 <mcyanek@gmail.com>
2020-04-08 15:15:30 +0200

    ongoing work on integration of projects modules

--- Area.java
+++ Area.java
@@ -1,4 +1,19 @@
+import java.util.ArrayList;
 
 public class Area {
-    
+    boolean wasVisited;
+    ArrayList<Entity> entityList;
+
+    Area(){        
+        this.wasVisited = true;
+        this.entityList = new ArrayList<Entity>();
+    }
+
+    public static char getSymbol(ArrayList<Entity> entityList){
+        for (Entity entity : entityList){ if (entity.isInstance(Character)) {return entity.getSymbol();}}            
+        for (Entity entity : entityList){ if (entity.isInstance(ObjectToPick)) {return entity.getSymbol();}}
+        for (Entity entity : entityList){ if (entity.isInstance(Terrain)) {return entity.getSymbol();}}
+        return '.';
+    }
+
 }
