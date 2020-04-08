4a2e522e5fb42d532b00db19c8272cbb395c5d6c
Neriso <Nerisox@gmail.com>
2020-04-08 12:27:26 +0200

    Terrian.java

---
+++ Terrian.java
@@ -0,0 +1,20 @@
+
+public class Terrian{
+
+    private String name;
+    private String flavorTxt;
+    
+    public void explore(){
+    
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    public String getFlavoerTxT(){
+        return this.flavorTxt;
+    }
+    
+    
+
+}
